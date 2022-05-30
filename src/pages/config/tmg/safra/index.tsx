08ad9b0cfdf106067b77f90f325b6f4aae352af7
Felipe Couto <couto.felipe2@gmail.com>
2022-05-30 16:51:58 -0300

    ocultando filtro por data da safra

--- src/pages/config/tmg/safra/index.tsx
+++ src/pages/config/tmg/safra/index.tsx
@@ -443,7 +443,7 @@ export default function Listagem({ allSafras, totalItems, itensPerPage, filterAp
                     />
                   </div>
 
-                  <div className="h-10 w-1/2 ml-4">
+                  {/* <div className="h-10 w-1/2 ml-4">
                     <label className="block text-gray-900 text-sm font-bold mb-2">
                       Data do início do plantio
                     </label>
@@ -464,9 +464,9 @@ export default function Listagem({ allSafras, totalItems, itensPerPage, filterAp
                           focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                         "
                     />
-                  </div>
+                  </div> */}
 
-                  <div className="h-10 w-1/2 ml-4">
+                  {/* <div className="h-10 w-1/2 ml-4">
                     <label className="block text-gray-900 text-sm font-bold mb-2">
                       Data do fim do plantio
                     </label>
@@ -487,7 +487,7 @@ export default function Listagem({ allSafras, totalItems, itensPerPage, filterAp
                           focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
                         "
                     />
-                  </div>
+                  </div> */}
                 </div>
 
                 <div className="h-16 w-32 mt-3">
