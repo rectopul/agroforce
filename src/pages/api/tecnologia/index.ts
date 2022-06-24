efd27e74484d07e70b3580d8bbfeca2d2539806f
Felipe Couto <couto.felipe2@gmail.com>
2022-06-24 16:56:22 -0300

    arrumando tecnologia e refatorando

--- src/pages/api/tecnologia/index.ts
+++ src/pages/api/tecnologia/index.ts
@@ -4,24 +4,24 @@ import { TecnologiaController } from '../../../controllers/tecnologia.controller
 import { apiHandler } from '../../../helpers/api';
 
 
-export default  apiHandler(handler);
+export default apiHandler(handler);
 
- async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const tecnologiaController =  new TecnologiaController();
-  switch (req.method) {
-    case 'GET':
-      const result = await tecnologiaController.getAll(req.query);
-      res.status(200).json(result);
-    break;
-    case 'POST':
-      const resultPost = await tecnologiaController.post(req.body);  
-      res.status(200).json(resultPost);
-    break;
-    case 'PUT':
-      const resultPut = await tecnologiaController.update(req.body);  
-      res.status(200).json(resultPut);
-    break;
-    default:
-      res.status(405).end(`Method ${req.method} Not Allowed`)
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+	const tecnologiaController = new TecnologiaController();
+	switch (req.method) {
+		case 'GET':
+			const result = await tecnologiaController.getAll(req.query);
+			res.status(result.status).json(result);
+			break;
+		case 'POST':
+			const resultPost = await tecnologiaController.create(req.body);
+			res.status(resultPost.status).json(resultPost);
+			break;
+		case 'PUT':
+			const resultPut = await tecnologiaController.update(req.body);
+			res.status(resultPut.status).json(resultPut);
+			break;
+		default:
+			res.status(405).end(`Method ${req.method} Not Allowed`)
+	}
 }
