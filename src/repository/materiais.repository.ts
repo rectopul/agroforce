c60d2a7a45f6d0c18769d1549880165deddd5d04
Felipe Couto <couto.felipe2@gmail.com>
2022-06-30 16:17:39 -0300

    fix bug

--- src/repository/materiais.repository.ts
+++ src/repository/materiais.repository.ts
@@ -1,69 +1,57 @@
 import { prisma } from '../pages/api/db/db';
 
 export class MateriaisRepository {
-    async create(data: any) {
-        const materiais = await prisma.materiais.create({ data });
-        return materiais;
-    }
-    async findOne(id: number) {
-        const materiais = await prisma.materiais.findUnique({
-            where: { id }
-        });
-        return materiais;
-    }
+	async create(data: any) {
+		const materiais = await prisma.materiais.create({ data });
+		return materiais;
+	}
+	async findOne(id: number) {
+		const materiais = await prisma.materiais.findUnique({
+			where: { id }
+		});
+		return materiais;
+	}
 
-    async update(id: number, data: any) {
-        const materiais = await this.findOne(id);
+	async update(id: number, data: any) {
+		const materiais = await this.findOne(id);
 
-        if (materiais !== null) {
-            const result = await prisma.materiais.update({
-                where: { id },
-                data
-            });
-            return result;
-        } else {
-            return false;
-        }
-    }
+		if (materiais !== null) {
+			const result = await prisma.materiais.update({
+				where: { id },
+				data
+			});
+			return result;
+		} else {
+			return false;
+		}
+	}
 
-    async list(id_experiment_bd: number) {
-        const materiais = await prisma.materiais.findMany({
-            where: { id_experiment_bd },
-            select: {
-                id: true,
-                id_d1: true,
-                id_dados: true,
-                id_experiment_bd: true,
-                id_l1: true,
-                name_genotipo: true,
-                name_main: true,
-                prox_nivel: true,
-                trat: true,
-                status: true
-            }
-        });
+	async list(id_experiment_bd: number) {
+		const materiais = await prisma.materiais.findMany({
+			where: { id_experiment_bd }
+		});
 
-        return materiais;
-    }
+		return materiais;
+	}
 
-    async findAll(where: any, select: any, take: any, skip: any, orderBy: string | any) {
-        let order: object | any;
+	async findAll(where: any, select: any, take: any, skip: any, orderBy: string | any) {
+		let order: object | any;
 
-        if (orderBy) {
-            order = JSON.parse(orderBy);
-        }
+		if (orderBy) {
+			order = JSON.parse(orderBy);
+		}
 
-        const count = await prisma.materiais.count({ where });
+		const count = await prisma.materiais.count({ where });
 
-        const result: object | any = await prisma.materiais.findMany({
-            select,
-            skip,
-            take,
-            where,
-            orderBy: order
-        });
+		const result: object | any = await prisma.materiais.findMany({
+			select,
+			skip,
+			take,
+			where,
+			orderBy: order
+		});
 
-        result.total = count;
-        return result;
-    }
+		result.total = count;
+		return result;
+	}
 }
