463958f159654c5c4da8356ecc4ad22b53f5528e
Felipe Couto <couto.felipe2@gmail.com>
2022-07-05 18:11:00 -0300

    ordenando quadra e ajustando local

--- src/controllers/local.controller.ts
+++ src/controllers/local.controller.ts
@@ -1,148 +1,141 @@
 import handleError from 'src/shared/utils/handleError';
 import { LocalRepository } from '../repository/local.repository';
+
 export class LocalController {
-	localRepository = new LocalRepository();
+  localRepository = new LocalRepository();
 
-	async getAll(options: object | any) {
-		const parameters: object | any = {};
-		let select: any = [];
-		try {
-			if (options.filterStatus) {
-				if (options.filterStatus != 2) parameters.status = Number(options.filterStatus);
-			}
+  async getAll(options: object | any) {
+    const parameters: object | any = {};
+    let select: any = [];
+    try {
+      if (options.filterStatus) {
+        if (options.filterStatus != 2) parameters.status = Number(options.filterStatus);
+      }
 
-			if (options.filterNameLocalCulture) {
-				parameters.name_local_culture = JSON.parse(`{ "contains":"${options.filterNameLocalCulture}" }`);
-			}
+      if (options.filterNameLocalCulture) {
+        parameters.name_local_culture = JSON.parse(`{ "contains":"${options.filterNameLocalCulture}" }`);
+      }
 
-			if (options.filterLabel) {
-				parameters.label = JSON.parse(`{ "contains":"${options.filterLabel}" }`);
-			}
+      if (options.filterLabel) {
+        parameters.label = JSON.parse(`{ "contains":"${options.filterLabel}" }`);
+      }
 
-			if (options.filterMloc) {
-				parameters.mloc = JSON.parse(`{ "contains":"${options.filterMloc}" }`);
-			}
+      if (options.filterMloc) {
+        parameters.mloc = JSON.parse(`{ "contains":"${options.filterMloc}" }`);
+      }
 
-			if (options.filterAdress) {
-				parameters.adress = JSON.parse(`{ "contains":"${options.filterAdress}" }`);
-			}
+      if (options.filterAdress) {
+        parameters.adress = JSON.parse(`{ "contains":"${options.filterAdress}" }`);
+      }
 
-			if (options.filterLabelCountry) {
-				parameters.label_country = JSON.parse(`{ "contains":"${options.filterLabelCountry}" }`);
-			}
+      if (options.filterLabelCountry) {
+        parameters.label_country = JSON.parse(`{ "contains":"${options.filterLabelCountry}" }`);
+      }
 
-			if (options.filterLabelRegion) {
-				parameters.label_region = JSON.parse(`{ "contains":"${options.filterLabelRegion}" }`);
-			}
+      if (options.filterLabelRegion) {
+        parameters.label_region = JSON.parse(`{ "contains":"${options.filterLabelRegion}" }`);
+      }
 
-			if (options.filterNameLocality) {
-				parameters.name_locality = JSON.parse(`{ "contains":"${options.filterNameLocality}" }`);
-			}
+      if (options.filterNameLocality) {
+        parameters.name_locality = JSON.parse(`{ "contains":"${options.filterNameLocality}" }`);
+      }
 
+      if (options.id_local_culture) {
+        parameters.id_local_culture = Number(options.id_local_culture);
+      }
 
-			if (options.id_local_culture) {
-				parameters.id_local_culture = Number(options.id_local_culture);
-			}
+      if (options.name_local_culture) {
+        parameters.name_local_culture = options.name_local_culture;
+      }
 
-			if (options.name_local_culture) {
-				parameters.name_local_culture = options.name_local_culture;
-			}
+      const take = (options.take) ? Number(options.take) : undefined;
 
-			const take = (options.take) ? Number(options.take) : undefined;
+      const skip = (options.skip) ? Number(options.skip) : undefined;
 
-			const skip = (options.skip) ? Number(options.skip) : undefined;
+      const orderBy = (options.orderBy) ? `{"${options.orderBy}":"${options.typeOrder}"}` : undefined;
 
-			const orderBy = (options.orderBy) ? `{"${options.orderBy}":"${options.typeOrder}"}` : undefined;
+      if (options.paramSelect) {
+        const objSelect = options.paramSelect.split(',');
+        Object.keys(objSelect).forEach((item) => {
+          select[objSelect[item]] = true;
+        });
+        select = { ...select };
+      } else {
+        select = {
+          id: true,
+          name_local_culture: true,
+          label: true,
+          mloc: true,
+          label_country: true,
+          label_region: true,
+          name_locality: true,
+          adress: true,
+          status: true,
+          cultureUnity: true,
+        };
+      }
 
-			if (options.paramSelect) {
-				let objSelect = options.paramSelect.split(',');
-				Object.keys(objSelect).forEach((item) => {
-					select[objSelect[item]] = true;
-				});
-				select = Object.assign({}, select);
-			} else {
-				select = {
-					id: true,
-					name_local_culture: true,
-					label: true,
-					mloc: true,
-					label_country: true,
-					label_region: true,
-					name_locality: true,
-					adress: true,
-					status: true,
-					cultureUnity: true
-				};
-			}
+      const response = await this.localRepository.findAll(
+        parameters,
+        select,
+        take,
+        skip,
+        orderBy,
+      );
+      if (!response || response.total <= 0) {
+        return { status: 400, response: [], total: 0 };
+      }
+      return { status: 200, response, total: response.total };
+    } catch (error: any) {
+      handleError('Local Controller', 'GetAll', error.message);
+      throw new Error('[Controller] - GetAll Local erro');
+    }
+  }
 
-			const response = await this.localRepository.findAll(parameters,
-				select,
-				take,
-				skip,
-				orderBy);
-			if (!response || response.total <= 0) {
-				return { status: 400, response: [], total: 0 };
-			} else {
-				return { status: 200, response: response, total: response.total };
-			}
-		} catch (error: any) {
-			handleError('Local Controller', 'GetAll', error.message)
-			throw new Error("[Controller] - GetAll Local erro")
-		}
-	}
+  async getOne({ id }: any) {
+    try {
+      if (id) {
+        const response = await this.localRepository.findOne(Number(id));
+        if (response) {
+          return { status: 200, response };
+        }
+        return { status: 404, response: [], message: 'Local não existe' };
+      }
+      return { status: 405, response: [], message: 'ID não recebido' };
+    } catch (error: any) {
+      handleError('Local Controller', 'GetOne', error.message);
+      throw new Error('[Controller] - GetOne Local erro');
+    }
+  }
 
-	async getOne({ id }: any) {
-		try {
-			if (id) {
-				const response = await this.localRepository.findOne(Number(id));
-				if (response) {
-					return { status: 200, response: response };
-				} else {
-					return { status: 404, response: [], message: 'Local não existe' };
-				}
-			} else {
-				return { status: 405, response: [], message: 'ID não recebido' };
-			}
-		} catch (error: any) {
-			handleError('Local Controller', 'GetOne', error.message)
-			throw new Error("[Controller] - GetOne Local erro")
-		}
-	}
+  async create(data: object | any) {
+    try {
+      const response = await this.localRepository.create(data);
+      if (response) {
+        return { status: 201, response, message: 'Local criado' };
+      }
+      return { status: 400, message: 'Erro ao criar local' };
+    } catch (error: any) {
+      handleError('Local Controller', 'Create', error.message);
+      throw new Error('[Controller] - Create Local erro');
+    }
+  }
 
-	async create(data: object | any) {
-		try {
+  async update(data: any) {
+    try {
+      const localCultura: any = await this.localRepository.findOne(data.id);
 
-			const response = await this.localRepository.create(data);
-			if (response) {
-				return { status: 201, response: response, message: "Local criado" }
-			} else {
-				return { status: 400, message: "Erro ao criar local" }
-			}
+      if (!localCultura) return { status: 404, message: 'Local de cultura não existente' };
 
-		} catch (error: any) {
-			handleError('Local Controller', 'Create', error.message)
-			throw new Error("[Controller] - Create Local erro")
-		}
-	}
+      const response = await this.localRepository.update(data.id, data);
 
-	async update(data: any) {
-		try {
-
-			const localCultura: any = await this.localRepository.findOne(data.id);
-
-			if (!localCultura) return { status: 404, message: 'Local de cultura não existente' };
-
-			const response = await this.localRepository.update(data.id, data);
-
-			if (response) {
-				return { status: 200, message: 'Local de cultura atualizado' };
-			} else {
-				return { status: 400, message: 'Não foi possível atualizar' };
-			}
-
-		} catch (error: any) {
-			handleError('Local Controller', 'Update', error.message)
-			throw new Error("[Controller] - Update Local erro")
-		}
-	}
+      if (response) {
+        return { status: 200, message: 'Local de cultura atualizado' };
+      }
+      return { status: 400, message: 'Não foi possível atualizar' };
+    } catch (error: any) {
+      handleError('Local Controller', 'Update', error.message);
+      throw new Error('[Controller] - Update Local erro');
+    }
+  }
 }
--- src/controllers/quadra.controller.ts
+++ src/controllers/quadra.controller.ts
@@ -1,138 +1,125 @@
-import { QuadraRepository } from "src/repository/quadra.repository";
-import handleOrderForeign from "src/shared/utils/handleOrderForeign";
+import handleError from '../shared/utils/handleError';
+import handleOrderForeign from '../shared/utils/handleOrderForeign';
+import { QuadraRepository } from '../repository/quadra.repository';
 
 export class QuadraController {
-	public readonly required = 'Campo obrigatório';
+  quadraRepository = new QuadraRepository();
 
-	quadraRepository = new QuadraRepository();
+  async getAll(options: any) {
+    const parameters: object | any = {};
+    let orderBy: object | any;
+    let select: any = [];
+    try {
+      if (options.filterStatus) {
+        if (options.filterStatus !== 2) parameters.status = Number(options.filterStatus);
+      }
 
-	async listAll(options: any) {
-		const parameters: object | any = {};
-		let take;
-		let skip;
-		let orderBy: object | any;
-		let select: any = [];
-		try {
-			if (options.filterStatus) {
-				if (typeof (options.status) === 'string') {
-					options.filterStatus = parseInt(options.filterStatus);
-					if (options.filterStatus != 2) parameters.status = parseInt(options.filterStatus);
-				} else {
-					if (options.filterStatus != 2) parameters.status = parseInt(options.filterStatus);
-				}
-			} else {
-				parameters.status = 1;
-			}
+      if (options.filterSearch) {
+        parameters.cod_quadra = JSON.parse(`{"contains":"${options.filterSearch}"}`);
+      }
 
-			if (options.filterSearch) {
-				options.filterSearch = '{"contains":"' + options.filterSearch + '"}';
-				// parameters.genotipo  = JSON.parse(options.filterSearch);
-				// parameters.cruza = JSON.parse(options.filterSearch);
-			}
+      if (options.paramSelect) {
+        const objSelect = options.paramSelect.split(',');
+        Object.keys(objSelect).forEach((item) => {
+          select[objSelect[item]] = true;
+        });
+        select = { ...select };
+      } else {
+        select = {
+          id: true,
+          cod_quadra: true,
+          local_plantio: true,
+          larg_q: true,
+          comp_p: true,
+          linha_p: true,
+          comp_c: true,
+          esquema: true,
+          tiro_fixo: true,
+          disparo_fixo: true,
+          q: true,
+          localPreparo: { select: { name_local_culture: true } },
+          Safra: { select: { safraName: true } },
+          status: true,
+        };
+      }
+      if (options.id_culture) {
+        parameters.id_culture = Number(options.id_culture);
+      }
 
-			if (options.paramSelect) {
-				let objSelect = options.paramSelect.split(',');
-				Object.keys(objSelect).forEach((item) => {
-					if (objSelect[item] === 'tecnologia') {
-						select[objSelect[item]] = true;
-					} else {
-						select[objSelect[item]] = true;
-					}
-				});
-				select = Object.assign({}, select);
-			} else {
-				select = { id: true, cod_quadra: true, local_plantio: true, larg_q: true, comp_p: true, linha_p: true, comp_c: true, esquema: true, tiro_fixo: true, disparo_fixo: true, q: true, localPreparo: { select: { name_local_culture: true } }, Safra: { select: { safraName: true } }, status: true };
-			}
-			if (options.id_culture) {
-				parameters.id_culture = parseInt(options.id_culture);
-			}
+      if (options.local_preparo) {
+        parameters.local_preparo = Number(options.local_preparo);
+      }
 
-			if (options.local_preparo) {
-				parameters.local_preparo = parseInt(options.local_preparo);
-			}
+      if (options.cod_quadra) {
+        parameters.cod_quadra = options.cod_quadra;
+      }
 
-			if (options.cod_quadra) {
-				parameters.cod_quadra = options.cod_quadra;
-			}
+      const take = (options.take) ? Number(options.take) : undefined;
 
-			if (options.take) {
-				if (typeof (options.take) === 'string') {
-					take = parseInt(options.take);
-				} else {
-					take = options.take;
-				}
-			}
+      const skip = (options.skip) ? Number(options.skip) : undefined;
 
-			if (options.skip) {
-				if (typeof (options.skip) === 'string') {
-					skip = parseInt(options.skip);
-				} else {
-					skip = options.skip;
-				}
-			}
+      if (options.orderBy) {
+        orderBy = handleOrderForeign(options.orderBy, options.typeOrder);
+        orderBy = orderBy || `{"${options.orderBy}":"${options.typeOrder}"}`;
+      }
 
-			if (options.orderBy) {
-				orderBy = handleOrderForeign(options.orderBy, options.typeOrder)
-				orderBy = orderBy ? orderBy : '{"' + options.orderBy + '":"' + options.typeOrder + '"}'
-			}
+      const response: object | any = await this.quadraRepository.findAll(
+        parameters,
+        select,
+        take,
+        skip,
+        orderBy,
+      );
 
+      if (!response && response.total <= 0) {
+        return {
+          status: 400, response: [], total: 0, message: 'nenhum resultado encontrado',
+        };
+      }
+      return { status: 200, response, total: response.total };
+    } catch (error: any) {
+      handleError('Quadra Controller', 'GetAll', error.message);
+      throw new Error('[Controller] - GetAll Quadra erro');
+    }
+  }
 
-			let response: object | any = await this.quadraRepository.findAll(
-				parameters,
-				select,
-				take,
-				skip,
-				orderBy
-			);
+  async getOne(id: number) {
+    try {
+      if (!id) throw new Error('Dados inválidos');
 
-			if (!response && response.total <= 0) {
-				return { status: 400, response: [], total: 0, message: 'nenhum resultado encontrado' };
-			} else {
-				return { status: 200, response, total: response.total }
-			}
-		} catch (err) {
-			console.log(err)
-			return { status: 400, response: [], total: 0 }
-		}
-	}
+      const response = await this.quadraRepository.findOne(id);
 
-	async getOne(id: number) {
-		try {
-			if (!id) throw new Error("Dados inválidos");
+      if (!response) throw new Error('Item não encontrado');
 
-			const response = await this.quadraRepository.findOne(id);
+      return { status: 200, response };
+    } catch (error: any) {
+      handleError('Quadra Controller', 'GetOne', error.message);
+      throw new Error('[Controller] - GetOne Quadra erro');
+    }
+  }
 
-			if (!response) throw new Error("Item não encontrado");
+  async create(data: any) {
+    try {
+      const response = await this.quadraRepository.create(data);
+      return { status: 201, message: 'Genealogia cadastrada', response };
+    } catch (error: any) {
+      handleError('Quadra Controller', 'Create', error.message);
+      throw new Error('[Controller] - Create Quadra erro');
+    }
+  }
 
-			return { status: 200, response };
-		} catch (err) {
-			return { status: 400, message: err }
-		}
-	}
+  async update(data: any) {
+    try {
+      const quadra = await this.quadraRepository.findOne(data.id);
 
-	async create(data: any) {
-		try {
-			let response = await this.quadraRepository.create(data);
-			return { status: 201, message: "Genealogia cadastrada", response }
-		} catch (err) {
-			console.log(err);
-			return { status: 400, message: "Erro no cadastrado" }
-		}
-	}
+      if (!quadra) return { status: 400, message: 'Genótipo não encontrado' };
 
-	async update(data: any) {
-		try {
+      await this.quadraRepository.update(quadra.id, data);
 
-			const quadra: any = await this.quadraRepository.findOne(data.id);
-
-			if (!quadra) return { status: 400, message: 'Genótipo não encontrado' };
-
-			await this.quadraRepository.update(quadra.id, data);
-
-			return { status: 200, message: "Genótipo atualizado" }
-		} catch (err) {
-			console.log(err);
-			return { status: 404, message: 'Erro ao atualizar' }
-		}
-	}
+      return { status: 200, message: 'Genótipo atualizado' };
+    } catch (error: any) {
+      handleError('Quadra Controller', 'Update', error.message);
+      throw new Error('[Controller] - Update Quadra erro');
+    }
+  }
 }
