2168c6c5952895b79dc556e4dee40b87bc754d33
Felipe Couto <couto.felipe2@gmail.com>
2022-08-11 17:25:25 -0300

    fix bug

--- src/shared/utils/calculatingExecutionTime.ts
+++ src/shared/utils/calculatingExecutionTime.ts
@@ -7,7 +7,7 @@ function millisecondsToMinutesAndSeconds(milliseconds: number) {
   }
   const minutes = Math.floor(milliseconds / 60000);
   const seconds = ((milliseconds % 60000) / 1000).toFixed(0);
-  return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  return `${minutes}:${(Number(seconds) < 10) ? '0' : ''}${seconds}`;
 }
 
 export default async function calculatingExecutionTime(id: number) {
