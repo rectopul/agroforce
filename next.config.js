fdb505f128d85d49323beb07a8984e66e42f14b1
adekzti <thiago@adekz.com>
2022-08-16 18:07:53 -0300

    k8s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  staticPageGenerationTimeout:100, 
   serverRuntimeConfig: {
     secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
   },
