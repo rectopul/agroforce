606f45e1e6b309ea2b39fb56f020a72a440e78ef
Stewart Cintra <stewartcintra@hotmail.com>
2022-08-23 08:55:57 -0300

    fazendo nova pagina de login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,165 @@
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { userService } from '../../services';
+
+import {
+  Container, Video, Content, Button, ContainerButton, TextButton, ContainerSocial, ContainerError,
+} from './styles';
+
+import Loading from '../../components/Loading';
+
+export default function Login() {
+  const router = useRouter();
+
+  const [hoverButton, setHoverButton] = useState(false);
+  const [hoverIconFB, setHoverIconFB] = useState(false);
+  const [hoverIconIG, setHoverIconIG] = useState(false);
+  const [hoverIconIN, setHoverIconIN] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // redirect to home if already logged in
+    if (userService.userValue) {
+      router.push('/');
+    }
+  }, [router]);
+
+  // form validation rules
+  const validationSchema = Yup.object().shape({
+    login: Yup.string().required('Login is required'),
+    password: Yup.string().required('Password is required'),
+  });
+  const formOptions = { resolver: yupResolver(validationSchema) };
+
+  // get functions to build form with useForm() hook
+  const {
+    register, handleSubmit, setError, formState,
+  } = useForm(formOptions);
+  const { errors }: any = formState;
+
+  async function onSubmit({ login, password }: any) {
+    setLoading(true);
+
+    await userService.login(login, password).then(() => {
+      // get return url from query parameters or default to '/'
+      const returnUrl = router.query.returnUrl || '/';
+      router.push(returnUrl as string);
+    })
+      .catch((error) => {
+        setError('apiError', { message: error });
+      });
+
+    setLoading(false);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      {loading && <Loading />}
+
+      <div style={Container}>
+        <video autoPlay loop muted style={Video}>
+          <source src="/images/qrcode_login.webm" type="video/webm" />
+        </video>
+
+        <div style={Content} className="shadow-lg">
+          <img src="/images/logo.png" className="w-30 h-14 self-center" />
+
+          {errors.apiError && (
+          <div style={ContainerError}>
+            <span style={{ color: '#fff' }}>
+              {errors.apiError?.message}
+            </span>
+          </div>
+          )}
+
+          <form style={{ width: '100%', marginTop: 10 }} onSubmit={handleSubmit(onSubmit)}>
+            <div style={{ height: 35 }}>
+              <input
+                {...register('login')}
+                placeholder="Usuário"
+                type="login"
+                name="login"
+                required
+                className="h-full w-full px-2 flex-1 block text-gray-900 rounded bg-white border-blue-600 focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+            <div style={{ height: 35, marginTop: 10, marginBottom: 10 }}>
+              <input
+                {...register('password')}
+                placeholder="Senha"
+                type="password"
+                name="password"
+                required
+                className="h-full w-full px-2 flex-1 block text-gray-900 rounded bg-white border-blue-600 focus:ring-indigo-500 focus:border-indigo-500"
+              />
+            </div>
+
+            <div style={ContainerButton}>
+              <Link href="/trocar-senha">
+                <a className="text-blue-600">Esqueci minha senha - I forgot my password - Olvide mi contraseña</a>
+              </Link>
+
+              <button
+                className="shadow-lg"
+                style={{ ...Button, backgroundColor: hoverButton ? '#fff' : '#609f51' }}
+                onMouseEnter={() => setHoverButton(true)}
+                onMouseLeave={() => setHoverButton(false)}
+              >
+                <span style={{ ...TextButton, color: hoverButton ? '#609f51' : '#fff' }}>Conectar</span>
+              </button>
+
+              <a href="https://agroforce.com.br/" style={{ marginTop: 10 }} target="_blank" rel="noreferrer">
+                <img src="/images/agroforce2.png" className="w-30 h-10 self-center" />
+              </a>
+
+              <div style={ContainerSocial}>
+                <a
+                  href="https://www.facebook.com/agroforcesoftware"
+                  title="Facebook"
+                  target="_blank"
+                  rel="noreferrer"
+                  onMouseEnter={() => setHoverIconFB(true)}
+                  onMouseLeave={() => setHoverIconFB(false)}
+                >
+                  <FaFacebook color={hoverIconFB ? '#609f51' : '#fff'} size={25} />
+                </a>
+                <a
+                  href="https://www.instagram.com/agroforcesoftware/"
+                  title="Instagram"
+                  target="_blank"
+                  rel="noreferrer"
+                  onMouseEnter={() => setHoverIconIG(true)}
+                  onMouseLeave={() => setHoverIconIG(false)}
+                >
+                  <FaInstagram color={hoverIconIG ? '#609f51' : '#fff'} size={25} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/company/agroforce-tecnologia/"
+                  title="Linkedin"
+                  target="_blank"
+                  rel="noreferrer"
+                  onMouseEnter={() => setHoverIconIN(true)}
+                  onMouseLeave={() => setHoverIconIN(false)}
+                >
+                  <FaLinkedin color={hoverIconIN ? '#609f51' : '#fff'} size={25} />
+                </a>
+              </div>
+
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/login/styles.js
@@ -0,0 +1,76 @@
+export const Container = {
+  display: 'flex',
+  width: '100vw',
+  height: '100vh',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+export const Video = {
+  top: -50,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  position: 'fixed',
+  width: '100%',
+  objectFit: 'cover',
+  zIndex: 0,
+};
+
+export const Content = {
+  display: 'flex',
+  flexDirection: 'column',
+  backgroundColor: 'rgba(150, 150, 150, 0.5)',
+  marginTop: 20,
+  paddingTop: 10,
+  paddingBottom: 20,
+  paddingLeft: 25,
+  paddingRight: 25,
+  width: '35%',
+  height: 380,
+  borderRadius: 30,
+  alignItems: 'center',
+  justifyContent: 'center',
+  zIndex: 1,
+};
+
+export const ContainerButton = {
+  display: 'flex',
+  flexDirection: 'column',
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+export const Button = {
+  width: 100,
+  height: 33,
+  borderRadius: 20,
+  marginTop: 10,
+  backgroundColor: '#609f51',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderWidth: 1,
+  borderColor: '#d3d3d3',
+};
+
+export const TextButton = {
+  color: '#fff',
+  fontWeight: '500',
+  fontSize: 15,
+};
+
+export const ContainerSocial = {
+  display: 'flex',
+  width: 120,
+  justifyContent: 'space-around',
+  marginTop: 10,
+};
+
+export const ContainerError = {
+  display: 'flex',
+  backgroundColor: '#F66E84',
+  width: '100%',
+  justifyContent: 'center',
+  borderRadius: 2,
+};
