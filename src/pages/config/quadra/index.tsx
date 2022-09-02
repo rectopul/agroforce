4b26da195fff2133c502231c68697d223f749216
Felipe Couto <couto.felipe2@gmail.com>
2022-09-02 15:58:02 -0300

    fix build bug

--- src/pages/config/quadra/index.tsx
+++ src/pages/config/quadra/index.tsx
@@ -636,51 +636,50 @@ export default function Listagem({
                   </div>
                 </div>
 
-                  <div className="h-6 w-1/2 ml-4">
-                    <label className="block text-gray-900 text-sm font-bold mb-1">
-                      Linha P
-                    </label>
-                    <div className="flex">
-                      <Input
-                        placeholder="De"
-                        id="filterPFrom"
-                        name="filterPFrom"
-                        onChange={formik.handleChange}
-                      />
-                      <Input
-                        style={{ marginLeft: 8 }}
-                        placeholder="Até"
-                        id="filterPTo"
-                        name="filterPTo"
-                        onChange={formik.handleChange}
-                      />
-                    </div>
-                  </div>
-
-                  <div className="h-10 w-1/2 ml-4">
-                    <label className="block text-gray-900 text-sm font-bold mb-1">
-                      Esquema
-                    </label>
+                <div className="h-6 w-1/2 ml-4">
+                  <label className="block text-gray-900 text-sm font-bold mb-1">
+                    Linha P
+                  </label>
+                  <div className="flex">
                     <Input
-                      type="text"
-                      placeholder="Esquema"
-                      max="40"
-                      id="filterSchema"
-                      name="filterSchema"
+                      placeholder="De"
+                      id="filterPFrom"
+                      name="filterPFrom"
+                      onChange={formik.handleChange}
+                    />
+                    <Input
+                      style={{ marginLeft: 8 }}
+                      placeholder="Até"
+                      id="filterPTo"
+                      name="filterPTo"
                       onChange={formik.handleChange}
                     />
                   </div>
+                </div>
 
-                  <div className="h-7 w-32 mt-6" style={{ marginLeft: 10 }}>
-                    <Button
-                      type="submit"
-                      onClick={() => { }}
-                      value="Filtrar"
-                      bgColor="bg-blue-600"
-                      textColor="white"
-                      icon={<BiFilterAlt size={20} />}
-                    />
-                  </div>
+                <div className="h-10 w-1/2 ml-4">
+                  <label className="block text-gray-900 text-sm font-bold mb-1">
+                    Esquema
+                  </label>
+                  <Input
+                    type="text"
+                    placeholder="Esquema"
+                    max="40"
+                    id="filterSchema"
+                    name="filterSchema"
+                    onChange={formik.handleChange}
+                  />
+                </div>
+
+                <div className="h-7 w-32 mt-6" style={{ marginLeft: 10 }}>
+                  <Button
+                    type="submit"
+                    onClick={() => { }}
+                    value="Filtrar"
+                    bgColor="bg-blue-600"
+                    textColor="white"
+                    icon={<BiFilterAlt size={20} />}
+                  />
                 </div>
               </form>
             </div>
