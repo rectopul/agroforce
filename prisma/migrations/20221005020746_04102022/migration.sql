e2d961a8d5fd4b8a0b6f282792889e32b8a9de0d
Felipe Couto <couto.felipe2@gmail.com>
2022-10-06 14:13:48 -0300

    fix build bugs

--- prisma/migrations/20221005020746_04102022/migration.sql
+++ prisma/migrations/20221005020746_04102022/migration.sql
@@ -1,2 +1,3 @@
 -- AddForeignKey
 ALTER TABLE `reportes` ADD CONSTRAINT `reportes_madeBy_fkey` FOREIGN KEY (`madeBy`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+experiment_genotipe experiment_genotipe[]
