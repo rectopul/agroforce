c7f4d271dd12b17ca30974142888636f94a34679
httppedro <pedvict0@gmail.com>
2022-11-04 11:24:38 -0300

    modified:   src/components/loaderComponent/loader.tsx 	modified:   src/pages/listas/experimentos/parcelas-experimento/index.tsx

--- src/components/loaderComponent/loader.tsx
+++ src/components/loaderComponent/loader.tsx
@@ -4,14 +4,11 @@ import rotate from '../../../public/images/Reload.svg';
 
 export default function Loader(){
   return(
-    <div width="100%">
     <Image
       src={rotate}
       alt="Loading"
       width="50%"
       height="50%"
     />
-    TESTE
-    </div>
   )
 }
