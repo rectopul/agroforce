dfe13a7383f564b41282dda53de2191def1a6394
Felipe Couto <couto.felipe2@gmail.com>
2022-12-14 22:57:56 -0300

    arrumando dnv as sub de parcelas

--- src/controllers/experiment-genotipe.controller.ts
+++ src/controllers/experiment-genotipe.controller.ts
@@ -20,7 +20,6 @@ export class ExperimentGenotipeController {
     const parameters: object | any = {};
     let orderBy: object | any;
     parameters.AND = [];
-    parameters.OR = [];
     try {
       if (options.filterFoco) {
         parameters.foco = JSON.parse(
@@ -73,7 +72,7 @@ export class ExperimentGenotipeController {
       }
 
       if (options.filterGli) {
-        parameters.OR.push(JSON.parse(`{ "experiment": { "assay_list": {"gli": {"contains": "${options.filterGli}" } } } }`));
+        parameters.AND.push(JSON.parse(`{ "experiment": { "assay_list": {"gli": {"contains": "${options.filterGli}" } } } }`));
       }
 
       if (options.filterStatus) {
@@ -97,14 +96,14 @@ export class ExperimentGenotipeController {
       }
 
       if (options.filterLocal) {
-        parameters.OR.push(JSON.parse(
+        parameters.AND.push(JSON.parse(
           `{ "experiment": { "local": { "name_local_culture": { "contains": "${options.filterLocal}" } } } }`,
         ));
       }
 
       if (options.filterDelineamento) {
-        parameters.OR.push(JSON.parse(
-          `{ "experiment": { "delineamento": { "name": { "contains": "${options.filterLocal}" } } } }`,
+        parameters.AND.push(JSON.parse(
+          `{ "experiment": { "delineamento": { "name": { "contains": "${options.filterDelineamento}" } } } }`,
         ));
       }
 
@@ -368,10 +367,6 @@ export class ExperimentGenotipeController {
 
       if (parameters.AND.length === 0) {
         delete parameters.AND;
-      }
-
-      if (parameters.OR.length === 0) {
-        delete parameters.OR;
       }
 
       const response: object | any = await this.ExperimentGenotipeRepository.findAll(
