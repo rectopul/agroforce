288d914309929dff2970d7fecb46202f1ed69627
Stewart Cintra <stewartcintra@hotmail.com>
2023-01-13 14:09:23 -0300

    feature para fixar o cabecalho da tabela

--- src/components/AccordionFilter/index.tsx
+++ src/components/AccordionFilter/index.tsx
@@ -1,25 +1,27 @@
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import * as React from 'react';
-import { ReactNode } from 'react';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import { ReactNode } from "react";
 
 interface IAccordionFilterProps {
   title?: string;
   children: ReactNode;
   grid?: boolean;
+  onChange?: (event: React.SyntheticEvent, expanded: boolean) => void;
 }
 
 export function AccordionFilter({
   title,
   children,
   grid = false,
+  onChange,
 }: IAccordionFilterProps) {
   return (
     <div className="w-full shadow-md rounded">
-      <Accordion className="w-full" defaultExpanded={grid}>
+      <Accordion className="w-full" defaultExpanded={grid} onChange={onChange}>
         <AccordionSummary
           style={{ paddingLeft: 10, paddingRight: 10 }}
           className="w-full"
