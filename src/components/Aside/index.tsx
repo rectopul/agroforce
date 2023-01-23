ed37d9614feda1921550a9cf1ca8608939affba7
Felipe Couto <couto.felipe2@gmail.com>
2023-01-23 16:06:23 -0300

    version 0.0.21.1

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -9,8 +9,7 @@ import { BsGear } from 'react-icons/bs';
 import { useRouter } from 'next/router';
 import { ButtonAside } from './ButtonAside';
 
-
-const versionApp = '0.0.20';
+const versionApp = '0.0.21.1';
 
 export function Aside({ moduloActive }: any) {
   return (
