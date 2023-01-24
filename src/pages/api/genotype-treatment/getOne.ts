ed0946edf5c1081b18826f225d7dd9d71949a46b
Douglas Inhani Martins <douglas@otix.com.br>
2023-01-24 16:30:24 -0300

    Aplicar a regra EM BACKEND de verificação de status dos experimentos do Genótipos do ensaio (genotype_treatment), se estiver com status_experiment == "EXP. SORTEADO" || status_experiment != "IMPORTADO" não permite continuar o processo de substituição.

--- src/pages/api/genotype-treatment/getOne.ts
+++ src/pages/api/genotype-treatment/getOne.ts
@@ -13,16 +13,12 @@ export const config = {
 export default apiHandler(handler);
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const genotypeTreatmentController = new GenotypeTreatmentController();
-  const { id } = req.query;
 
   switch (req.method) {
-    case 'GET':
-      const result = await genotypeTreatmentController.getOne({id: Number(id)});
-      res.status(200).json(result.response);
-      break;
-    case 'PUT':
-      const resultPut = await genotypeTreatmentController.update(req.body);
-      res.status(200).json(resultPut);
+    case 'POST':
+      console.log('req.body',req.body, req.body?.id);
+      const result = await genotypeTreatmentController.getOne(Number(req.body?.id));
+      res.status(200).json(result);
       break;
     default:
       res.status(405).end(`Method ${req.method} Not Allowed`);
