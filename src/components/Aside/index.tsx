6a7d20ea5e13a0f9fd987378852d90925e7a6c6b
Douglas Inhani Martins <douglas@otix.com.br>
2023-01-27 11:36:46 -0300

    version 0.0.22.3 fix build

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -9,7 +9,7 @@ import { BsGear } from 'react-icons/bs';
 import { useRouter } from 'next/router';
 import { ButtonAside } from './ButtonAside';
 
-const versionApp = '0.0.22.3';
+const versionApp = '0.0.22.3'; // fix
 
 export function Aside({ moduloActive }: any) {
   const aArray = versionApp.split('.');
