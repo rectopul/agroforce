eae850e559fd49e4a1ac4436e3f6e0f70124b0a8
Douglas Inhani Martins <douglas@otix.com.br>
2023-02-02 12:50:03 -0300

    version 0.0.23.5

---
+++ prisma/migrations/20230202154648_v0_0_23_5_rollback/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `xxx` on the `log_import` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `log_import` DROP COLUMN `xxx`;
