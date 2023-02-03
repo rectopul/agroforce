243250a99d13ca7902e0ace0b4d47579dd5c2977
Stewart Cintra <stewartcintra@hotmail.com>
2023-02-03 16:08:48 -0300

    pontuacao milhar na etiqueta no npe e nca

--- src/components/TagPrint/index.tsx
+++ src/components/TagPrint/index.tsx
@@ -1,18 +1,18 @@
-import Head from "next/head";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { useQRCode } from "next-qrcode";
-import { IoMdArrowBack } from "react-icons/io";
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useQRCode } from 'next-qrcode';
+import { IoMdArrowBack } from 'react-icons/io';
 
-import { functionsUtils } from "../../shared/utils/functionsUtils";
+import { functionsUtils } from '../../shared/utils/functionsUtils';
 
-import styles from "./styles.module.css";
-import Etiqueta from "./etiqueta";
-import BarCode from "./barcode";
+import styles from './styles.module.css';
+import Etiqueta from './etiqueta';
+import BarCode from './barcode';
 
-import maoDireita from "../../../public/images/mao-direito.png";
-import maoEsquerda from "../../../public/images/mao-esquerdo.png";
-import algodao from "../../../public/images/algodao.png";
+import maoDireita from '../../../public/images/mao-direito.png';
+import maoEsquerda from '../../../public/images/mao-esquerdo.png';
+import algodao from '../../../public/images/algodao.png';
 
 interface TagPrintProps {
   tagType: number;
@@ -26,7 +26,7 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
   const random = (max = 30, min = 10) => Math.floor(Math.random() * max) + min;
 
   function generateEAN8(npe: any) {
-    let ean8 = "";
+    let ean8 = '';
     let npe7Digits = String(npe);
     let countNPE = String(npe)?.length;
 
@@ -45,7 +45,7 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
     const listTags = data.map((item: any) => (
       <div className="etiquetaModelo1">
         <div className="flexRow" style={{ fontSize: 16 }}>
-          <div className="flex1">{item?.npe}</div>
+          <div className="flex1">{Number(item?.npe)?.toLocaleString('pt-BR')}</div>
           <div className="flex1">
             {item?.npe && <BarCode valor={generateEAN8(item?.npe)} />}
           </div>
@@ -65,12 +65,26 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
           </div>
           {/* FIM CONTAGEM IMPRESSAO ETIQUETAS */}
         </div>
+        {/* <div
+          className="flexRow"
+          style={{
+            fontSize: 7,
+            justifyContent: 'flex-end',
+            marginTop: -7,
+            marginBottom: -4,
+          }}
+        >
+          <div>{generateEAN8(item?.npe)}</div>
+        </div> */}
         <div className="flexRow" style={{ fontSize: 9 }}>
           <div className="flex1">{item?.genotipo?.name_genotipo}</div>
-          <div>{item?.nca}</div>
+          <div>{Number(item?.nca)?.toLocaleString('pt-BR')}</div>
         </div>
         <div style={{ fontSize: 7.5 }}>
-          {item?.experiment?.local?.name_local_culture}-{item?.gli}-
+          {item?.experiment?.local?.name_local_culture}
+          -
+          {item?.gli}
+          -
           {item?.type_assay?.envelope[0]?.seeds}
           SMT
         </div>
@@ -78,9 +92,9 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
     ));
 
     return (
-      <div className="todasEtiquetas" style={{ backgroundColor: "white" }}>
+      <div className="todasEtiquetas" style={{ backgroundColor: 'white' }}>
         <div className="notPrint">
-          <div style={{ justifyContent: "flex-start" }}>
+          <div style={{ justifyContent: 'flex-start' }}>
             <a
               onClick={() => router.back()}
               className={styles.card}
@@ -118,16 +132,19 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
               width={50}
               height={35}
             />
-            <div style={{ fontSize: 24 }}>{random(0, 9)}A</div>
+            <div style={{ fontSize: 24 }}>
+              {random(0, 9)}
+              A
+            </div>
           </div>
         </div>
       </div>
     ));
 
     return (
-      <div className="todasEtiquetas" style={{ backgroundColor: "white" }}>
+      <div className="todasEtiquetas" style={{ backgroundColor: 'white' }}>
         <div className="notPrint">
-          <div style={{ justifyContent: "flex-start" }}>
+          <div style={{ justifyContent: 'flex-start' }}>
             <a
               onClick={() => router.back()}
               className={styles.card}
@@ -147,16 +164,16 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
     const listTags = data.map(() => (
       <div className="etiquetaModelo3">
         <div className="flexColumn" style={{ fontSize: 16 }}>
-          <div style={{ textAlign: "center", fontSize: 34 }}>MTG</div>
-          <div style={{ textAlign: "center", fontSize: 50 }}>HORTA</div>
-          <div style={{ textAlign: "center", fontSize: 26 }}>QM-31</div>
-          <div style={{ textAlign: "center", fontSize: 22 }}>
+          <div style={{ textAlign: 'center', fontSize: 34 }}>MTG</div>
+          <div style={{ textAlign: 'center', fontSize: 50 }}>HORTA</div>
+          <div style={{ textAlign: 'center', fontSize: 26 }}>QM-31</div>
+          <div style={{ textAlign: 'center', fontSize: 22 }}>
             16X04(P4)T-2021
           </div>
-          <div style={{ textAlign: "center", fontSize: 30 }}>CX:1A</div>
+          <div style={{ textAlign: 'center', fontSize: 30 }}>CX:1A</div>
           <div
             style={{
-              position: "absolute",
+              position: 'absolute',
               left: 0,
               bottom: 0,
             }}
@@ -164,25 +181,25 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
             <Canvas
               text={`${random(
                 999999999999,
-                100000000000
+                100000000000,
               )}TEXTO_MTG_HORTA_QM-31`}
               options={{
-                type: "image/jpeg",
+                type: 'image/jpeg',
                 quality: 0.3,
-                level: "M",
+                level: 'M',
                 margin: 3,
                 scale: 4,
                 width: 80,
                 color: {
-                  dark: "#000000FF",
-                  light: "#FFFFFFFF",
+                  dark: '#000000FF',
+                  light: '#FFFFFFFF',
                 },
               }}
             />
           </div>
           <div
             style={{
-              position: "absolute",
+              position: 'absolute',
               right: 0,
               bottom: 0,
             }}
@@ -191,10 +208,10 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
           </div>
           <div
             style={{
-              position: "absolute",
+              position: 'absolute',
               right: 0,
               top: 0,
-              textAlign: "center",
+              textAlign: 'center',
             }}
           >
             <Image src={algodao} width={50} height={50} />
@@ -206,9 +223,9 @@ function TagPrint({ tagType = 1, data = [] }: TagPrintProps) {
     ));
 
     return (
-      <div className="todasEtiquetas" style={{ backgroundColor: "white" }}>
+      <div className="todasEtiquetas" style={{ backgroundColor: 'white' }}>
         <div className="notPrint">
-          <div style={{ justifyContent: "flex-start" }}>
+          <div style={{ justifyContent: 'flex-start' }}>
             <a
               onClick={() => router.back()}
               className={styles.card}
