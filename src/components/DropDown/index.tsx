72e3f7b0194f03cb738648f318e7a9b61eac0bc0
Stewart Cintra <stewartcintra@hotmail.com>
2023-03-03 15:24:09 -0300

    teste para não exibir menu dropdown segundo permissoes

--- src/components/DropDown/index.tsx
+++ src/components/DropDown/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import Link from 'next/link';
-import { ReactNode } from 'react';
-import perm_can_do from '../../shared/utils/perm_can_do';
+import Link from "next/link";
+import { ReactNode } from "react";
+import perm_can_do from "../../shared/utils/perm_can_do";
 
 interface IDropDownProps {
   label: string;
@@ -9,37 +9,41 @@ interface IDropDownProps {
   icon: string | ReactNode;
 }
 
-export function DropDown({
-  label, href, icon,
-}: IDropDownProps) {
+export function DropDown({ label, href, icon }: IDropDownProps) {
   return (
-    <ul
-      className="
+    <>
+      {perm_can_do(`${href}`, "view") ? (
+        <ul
+          className="
       h-7
       px-2
       bg-white mb-0.5
     "
-      style={{ maxWidth: '300px' }}
-    >
-      <li>
-        <Link href={href}>
-          <a
-            className="
-            flex
-            items-center
-            gap-2
-            text-base
-            text-gray-900
-            duration-300
-            hover:text-blue-600
-            "
-            href={!perm_can_do(`${href}`, 'view') ? href : '#'}
-          >
-            {icon}
-            {label}
-          </a>
-        </Link>
-      </li>
-    </ul>
+          style={{ maxWidth: "300px" }}
+        >
+          <li>
+            <Link href={href}>
+              <a
+                className="
+              flex
+              items-center
+              gap-2
+              text-base
+              text-gray-900
+              duration-300
+              hover:text-blue-600
+              "
+                href={href}
+              >
+                {icon}
+                {label}
+              </a>
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
