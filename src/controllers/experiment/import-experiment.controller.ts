af49c94fd6d6d395e0ca084d02f28c37fde7ad8a
Felipe Couto <couto.felipe2@gmail.com>
2023-03-23 09:38:55 -0300

    experiment

--- src/controllers/experiment/import-experiment.controller.ts
+++ src/controllers/experiment/import-experiment.controller.ts
@@ -102,9 +102,8 @@ export class ImportExperimentController {
               await logImportController.update({
                 id: idLog, status: 1, state: 'INVALIDA', updated_at: new Date(Date.now()), invalid_data: `Erro na linha ${Number(row) + 1}. Experimento já cadastrado e utilizado no sistema`,
               });
-              return { status: 200, message: `Erro na linha ${Number(row) + 1}. Experimento já cadastrado e utilizado no sistema` };
-              // responseIfError[0]
-              // += `<li style="text-align:left"> Erro na linha ${Number(row) + 1}. Experimento já cadastrado e utilizado no sistema </li> <br>`;
+              responseIfError[0]
+              += `<li style="text-align:left"> Erro na linha ${Number(row) + 1}. Experimento já cadastrado e utilizado no sistema </li> <br>`;
             }
           }
           if (experimentNameTemp.includes(experimentName)) {
@@ -113,9 +112,8 @@ export class ImportExperimentController {
             });
 
             experimentNameTemp[row] = experimentName;
-            return { status: 200, message: `Erro na linha ${Number(row) + 1}. Experimentos duplicados na tabela` };
-            // responseIfError[0]
-            //   += `<li style="text-align:left"> Erro na linha ${Number(row) + 1}. Experimentos duplicados na tabela </li> <br>`;
+            responseIfError[0]
+              += `<li style="text-align:left"> Erro na linha ${Number(row) + 1}. Experimentos duplicados na tabela </li> <br>`;
           }
           experimentNameTemp[row] = experimentName;
           let assayList: any = {};
