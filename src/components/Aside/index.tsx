59d8aa635e289ff35d979b469656274c8c0cc6f8
Douglas Inhani Martins <douglas@otix.com.br>
2023-07-04 19:11:37 -0300

    version: 0.0.49.2

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -12,7 +12,7 @@ import { useEffect, useState } from 'react';
 import { ButtonAside } from './ButtonAside';
 import { asidePermissions } from '../../shared/utils/perm_can_do';
 
-const versionApp = '0.0.49.1';
+const versionApp = '0.0.49.2';
 
 export function Aside({ moduloActive }: any) {
   
