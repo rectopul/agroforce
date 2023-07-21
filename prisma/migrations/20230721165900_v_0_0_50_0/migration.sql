cb7424dd5dc522f02a36bcfd5b3023dd37c0fef8
Douglas Inhani Martins <douglas@otix.com.br>
2023-07-21 13:59:55 -0300

    version: 0.0.50.0

---
+++ prisma/migrations/20230721165900_v_0_0_50_0/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `cultureUnity` ADD COLUMN `dt_rde` DECIMAL(20, 10) NULL;
+
+-- AlterTable
+ALTER TABLE `lote` ADD COLUMN `dt_rde` DECIMAL(20, 10) NULL;
+
+-- AlterTable
+ALTER TABLE `tecnologia` ADD COLUMN `dt_rde` DECIMAL(20, 10) NULL;
